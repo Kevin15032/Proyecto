9c73c7d26022e99d96323388de49050c1f0626bd
Kevin15032 <kdtorres1503@gmail.com>
2024-10-22 15:26:02 -0600

    primera fase

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <div class="container mt-5 col-md-6">
+    <h1>Nombre:Domínguez Torres Kevin</h1>
+    <h1>Grupo:s193</h1>
+    <h1>Materia: Programación web</h1>
+    <a href=""  class="btn btn-primary">REPASO 1</a>
+     </div>
+</body>
+</html>
